06befbe1d36b395a242b6da398ec775dcfa9fb56
Anokhin Oleg <olegyurievich1991@gmail.com>
2018-07-20 09:41:59 +0300

    enabling sass and sourcemaping

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,24 @@
 var Encore = require('@symfony/webpack-encore');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 Encore
 // the project directory where all compiled assets will be stored
     .setOutputPath('public/build/')
     // the public path used by the web server to access the previous directory
     .setPublicPath('/build')
-    .addEntry('rep_log', './public/assets/js/rep_log.js')
-    .addEntry('login', './public/assets/js/login.js')
-    .addEntry('layout', './public/assets/js/layout.js')
+    .addEntry('rep_log', './assets/js/rep_log.js')
+    .addEntry('login', './assets/js/login.js')
+    .addEntry('layout', './assets/js/layout.js')
     .enableBuildNotifications()
     // fixes modules that expect jQuery to be global
     .autoProvidejQuery()
+    .enableSassLoader()
+    .enableSourceMaps(!Encore.isProduction())
+    .addPlugin(new CopyWebpackPlugin([
+        {
+            from: './assets/static', to: 'static'
+        }
+    ]))
 ;
 
 
@@ -19,7 +27,6 @@ Encore
 
 // allow sass/scss files to be processed
 // .enableSassLoader()
-;
 
 // export the final configuration
 module.exports = Encore.getWebpackConfig();
