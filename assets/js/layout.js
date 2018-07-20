4645da10e67613da9e39481053b3a424ba4cc6df
Anokhin Oleg <olegyurievich1991@gmail.com>
2018-07-20 17:33:26 +0300

    starting with react

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -5,7 +5,7 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
 import '../css/main.scss';
-console.log('lol');
+
 $(document).ready(function() {
     $('[data-toggle="tooltip"]').tooltip();
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,26 @@
 var Encore = require('@symfony/webpack-encore');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-
 Encore
 // the project directory where all compiled assets will be stored
     .setOutputPath('public/build/')
     // the public path used by the web server to access the previous directory
     .setPublicPath('/build')
-
     .createSharedEntry('layout', './assets/js/layout.js')
     .addEntry('rep_log', './assets/js/rep_log.js')
+    .addEntry('rep_log_react', './assets/js/rep_log_react.js')
     .addEntry('login', './assets/js/login.js')
-
     .enableBuildNotifications()
     // fixes modules that expect jQuery to be global
     .autoProvidejQuery()
+    .addPlugin(new CopyWebpackPlugin([
+        // copies to {output}/static
+        { from: './assets/static', to: 'static' }
+    ]))
     .enableSassLoader()
     .enableSourceMaps(!Encore.isProduction())
-    .addPlugin(new CopyWebpackPlugin([
-        {
-            from: './assets/static', to: 'static'
-        }
-    ]))
     .cleanupOutputBeforeBuild()
-    .enableVersioning()
+    .enableVersioning(Encore.isProduction())
+    .enableReactPreset()
 ;
-
-
-// create hashed filenames (e.g. app.abc123.css)
-// .enableVersioning()
-
-// allow sass/scss files to be processed
-// .enableSassLoader()
-
 // export the final configuration
 module.exports = Encore.getWebpackConfig();
