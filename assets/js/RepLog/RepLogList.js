193df81a85e700846b398e2db941b25ba51ba408
Anokhin Oleg <olegyurievich1991@gmail.com>
2018-07-27 14:21:09 +0300

    dumb and smart component separation

--- assets/js/RepLog/RepLogList.js
+++ assets/js/RepLog/RepLogList.js
@@ -1,29 +1,34 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-export default class RepLogList extends Component {
+export default function RepLogList(props) {
 
-    render() {
-        const { highlightedRowId, onRowClick } = this.props;
-        const repLogs = [
-            { id: 1, reps: 25, itemLabel: 'My Laptop', totalWeightLifted: 112.5 },
-            { id: 2, reps: 10, itemLabel: 'Big Fat Cat', totalWeightLifted: 180 },
-            { id: 8, reps: 4, itemLabel: 'Big Fat Cat', totalWeightLifted: 72 }
-        ];
-        return (
-            <tbody>
-            {repLogs.map((repLog) => (
-                <tr
-                    key={repLog.id}
-                    className={highlightedRowId === repLog.id ? 'info' : ''}
-                    onClick={() => onRowClick(repLog.id)}
-                >
-                    <td>{repLog.itemLabel}</td>
-                    <td>{repLog.reps}</td>
-                    <td>{repLog.totalWeightLifted}</td>
-                    <td>...</td>
-                </tr>
-            ))}
-            </tbody>
-        );
-    }
+    const { highlightedRowId, onRowClick } = props;
+    const repLogs = [
+        { id: 1, reps: 25, itemLabel: 'My Laptop', totalWeightLifted: 112.5 },
+        { id: 2, reps: 10, itemLabel: 'Big Fat Cat', totalWeightLifted: 180 },
+        { id: 8, reps: 4, itemLabel: 'Big Fat Cat', totalWeightLifted: 72 }
+    ];
+    return (
+        <tbody>
+        {repLogs.map((repLog) => (
+            <tr
+                key={repLog.id}
+                className={highlightedRowId === repLog.id ? 'info' : ''}
+                onClick={() => onRowClick(repLog.id)}
+            >
+                <td>{repLog.itemLabel}</td>
+                <td>{repLog.reps}</td>
+                <td>{repLog.totalWeightLifted}</td>
+                <td>...</td>
+            </tr>
+        ))}
+        </tbody>
+    );
+
+}
+
+RepLogList.propTypes = {
+    highlightedRowId: PropTypes.any,
+    onRowClick: PropTypes.func.isRequired
 }
